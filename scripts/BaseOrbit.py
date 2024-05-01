dcd6f35c6fc9e86d3fe7d6d89f83c7a65deea780
Alex Khosrowshahi <alexander_khosrowshahi@brown.edu>
2024-05-01 13:41:55 -0400

    fix: jupyter reqs

--- scripts/BaseOrbit.py
+++ scripts/BaseOrbit.py
@@ -1,14 +1,15 @@
 from astropy import units as u
-from astropy import time
+from astropy.time import Time
 
 import numpy as np
 
 from poliastro import iod
-from poliastro.bodies import Earth
+from poliastro.bodies import Earth, Moon
+from poliastro.frames import Planes
 from poliastro.ephem import Ephem
 from poliastro.maneuver import Maneuver
 from poliastro.twobody import Orbit
-from poliastro.plotting import OrbitPlotter3D
+from poliastro.plotting import OrbitPlotter
 from poliastro.util import time_range
 from poliastro.spacecraft import Spacecraft
 # More info: https://plotly.com/python/renderers/
@@ -16,5 +17,10 @@ import plotly.io as pio
 pio.renderers.default = "plotly_mimetype+notebook_connected"
 from astropy.coordinates import solar_system_ephemeris
 
+epoch = Time("2024-05-01 13:05:50", scale = "tdb")
+
+plotter = OrbitPlotter(plane=Planes.EARTH_ECLIPTIC)
+plotter.plot_body_orbit(Earth, epoch, label = "Earth")
+plotter.plot_body_orbit(Moon, epoch, label = "Moon")
 
 
